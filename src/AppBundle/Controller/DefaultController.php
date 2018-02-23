6536076075570d75ba1b4b298cadaedc8a64128b
Karim <abdounikarim@gmail.com>
2018-02-23 11:52:57 +0100

    form submit

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,9 +2,12 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Enclosure;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Factory\DinosaurFactory;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 
 class DefaultController extends Controller
 {
@@ -22,5 +25,25 @@ class DefaultController extends Controller
         ]);
     }
 
-
+    /**
+     * @Route("/grow", name="grow_dinosaur")
+     * @Method({"POST"})
+     */
+    public function growAction(Request $request, DinosaurFactory $dinosaurFactory)
+    {
+        $manager = $this->getDoctrine()->getManager();
+        $enclosure = $manager->getRepository(Enclosure::class)
+            ->find($request->request->get('enclosure'));
+        $specification = $request->request->get('specification');
+        $dinosaur = $dinosaurFactory->growFromSpecification($specification);
+        $dinosaur->setEnclosure($enclosure);
+        $enclosure->addDinosaur($dinosaur);
+        $manager->flush();
+        $this->addFlash('success', sprintf(
+            'Grew a %s in enclosure #%d',
+            mb_strtolower($specification),
+            $enclosure->getId()
+        ));
+        return $this->redirectToRoute('homepage');
+    }
 }
