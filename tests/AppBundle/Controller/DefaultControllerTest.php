d98601ad0a618f3275b0d35f880eb940371081b6
Karim <abdounikarim@gmail.com>
2018-02-23 11:16:59 +0100

    fixtures references

--- tests/AppBundle/Controller/DefaultControllerTest.php
+++ tests/AppBundle/Controller/DefaultControllerTest.php
@@ -24,4 +24,20 @@ class DefaultControllerTest extends WebTestCase
 
         $this->assertCount(3, $table->filter('tbody tr'));
     }
+
+    public function testThatThereIsAnAlarmButtonWithoutSecurity()
+    {
+        $fixtures = $this->loadFixtures([
+            LoadSecurityData::class,
+            LoadBasicParkData::class,
+        ])->getReferenceRepository();
+
+        $client = $this->makeClient();
+        $crawler = $client->request('GET', '/');
+
+        $enclosure = $fixtures->getReference('carnivorous-enclosure');
+        $selector = sprintf('#enclosure-%s .button-alarm', $enclosure->getId());
+
+        $this->assertGreaterThan(0, $crawler->filter($selector)->count());
+    }
 }
